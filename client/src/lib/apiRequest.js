57fb5a394ba26d6ec2f431bd16adc713fac7eac2
Yash Gupta <yashgupta13.in@gmail.com>
2024-05-27 01:32:19 +0530

    updated baseURL
    Hosted Backend on Render Successfully

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://rentify-real-estate-application.onrender.com/api",
   withCredentials: true,
 });
 
