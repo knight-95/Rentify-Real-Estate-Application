82644f83f42889703314bab58e68181bf408dabd
Yash Gupta <79582395+knight-95@users.noreply.github.com>
2024-05-28 01:07:04 +0530

    Update homePage.jsx
    Added data for Kolkata city

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -15,21 +15,9 @@ function HomePage() {
           <p>
           Explore a wide range of properties for buying and renting. From cozy apartments to spacious family homes, we have the perfect match for your lifestyle and budget.
           </p>
+          <p style={{color:"red"}}>*Search results are available for <span style={{fontWeight:"bold"}}>Kolkata City</span>, more to be added soon....</p>
           <SearchBar />
-          {/* <div className="boxes">
-            <div className="box">
-              <h1>16+</h1>
-              <h2>Years of Experience</h2>
-            </div>
-            <div className="box">
-              <h1>200</h1>
-              <h2>Award Gained</h2>
-            </div>
-            <div className="box">
-              <h1>2000+</h1>
-              <h2>Property Ready</h2>
-            </div>
-          </div> */}
+         
         </div>
       </div>
       <div className="imgContainer">
